7ae4e2740db5993693c06ddfab8319b52a571410
giri79972 <48755929+giri79972@users.noreply.github.com>
2019-05-24 15:12:02 +0530

    Add files via upload

---
+++ MTO/MTOAPP/admin.py
@@ -0,0 +1,13 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Student,Course
+# Register your models here.
+
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['sno','sname','sloc']
+
+class AdminCourse(admin.ModelAdmin):
+    list_display = ['cno','cname','cfee']
+
+admin.site.register(Student,AdminStudent)
+admin.site.register(Course,AdminCourse)
